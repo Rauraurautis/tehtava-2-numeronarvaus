ffd1c8646fee832ff42ece64468bddfce9bd84fb
unknown <rautanokka@gmail.com>
2022-01-18 17:40:59 +0200

    Tehtävä tehty

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
+import React, { useState, useMemo, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+  const [randomNumber, setRandomNumber] = useState(() => { return Math.floor((Math.random() * 100) + 1) })
+  const [number, setNumber] = useState(null)
+  const [tries, setTries] = useState(0)
+  const [text, setText] = useState("Guess a number between 1-100")
+
+  const compareNumbers = () => {
+    if (number < 1 || number > 100) {
+      return Alert.alert("Enter a number between 1-100")
+    }
+
+    if (Number(number) === randomNumber) {
+      Alert.alert(`You guessed the number in ${tries + 1} guess(es)!`)
+      setNumber(null)
+      setRandomNumber(() => { return Math.floor((Math.random() * 100) + 1) })
+      setText("Guess a number between 1-100")
+      setTries(0)
+      return
+    }
+    setTries(tries + 1)
+    number < randomNumber ? setText(`Your guess ${number} is too low`) : setText(`Your guess ${number} is too high`)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{text}</Text>
+      <TextInput style={styles.numberinput} keyboardType='numeric' onChangeText={text => {
+        setNumber(text)
+      }} value={number} />
+      <View style={styles.button}>
+        <Button onPress={compareNumbers} title="Make a guess" />
+      </View>
     </View>
   );
 }
@@ -17,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  numberinput: {
+    borderWidth: 2,
+    width: 250
+  },
+  button: {
+    marginTop: 10
+  }
 });
